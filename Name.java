7fbe04ad6861d2a33f812343873226cd57293a31
Ankit Tiwari <ankittiwari0693@gmail.com>
2016-11-25 20:35:26 +0530

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,7 @@
+class Name
+ { 
+      public static void main(String...a[]) 
+  { 
+      System.out.println("Ankit Tiwari");
+   }
+}
---
+++ Table.java
@@ -0,0 +1,11 @@
+﻿class A { 
+   public static void main(String[] p) { 
+    int a=2; 
+    int i,r;
+for(i=1; i<=10; i++)
+{
+r=a*i;
+System.out.println(r);
+}
+   }
+}
